3037c6aba7df6e2eca1c7a688d628df0d8c82d21
nivel10 <carlos.e.herrera.j@gmail.com>
2017-09-18 17:00:29 -0400

    App Version 1.0.0 - 18/09/2017 - 5:00

--- ForeignExchangeMac/ViewModels/MainViewModel.cs
+++ ForeignExchangeMac/ViewModels/MainViewModel.cs
@@ -1,13 +1,26 @@
 ﻿namespace ForeignExchangeMac.ViewModels
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
     using Models;
+    using Newtonsoft.Json;
+    using Xamarin.Forms;
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Attributes
+
+        private bool _isRunning;
+        private string _result;
+        private bool _isEnabled;
+        private ObservableCollection<Rate> _rates;
+
+        #endregion
+
         #region Commands
 
         public ICommand ConvertCommand
@@ -18,11 +31,17 @@
             }
         }
 
-        #endregion
+		#endregion
 
-        #region Properties
+		#region Events
 
-        public string Amount
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Properties
+
+		public string Amount
         {
             get;
             set;
@@ -30,9 +49,19 @@
 
         public ObservableCollection<Rate> Rates
         {
-            get;
-            set;
-        }
+            get
+			{
+                return _rates;
+            }
+			set
+			{
+                if(value != _rates)
+				{
+                    _rates = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Rates)));
+                }
+			}
+		}
 
         public Rate SourceRate
         {
@@ -48,21 +77,52 @@
 
         public bool IsRunning
         {
-            get;
-            set;
-        }
+            get
+            {
+                return _isRunning;
+            }
+			set
+			{
+                if(value != _isRunning)
+				{
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+			}
+		}
 
         public bool IsEnabled
         {
-            get;
-            set;
-        }
+            get
+			{
+                return _isEnabled;
+            }
+
+			set
+			{
+                if(value != _isEnabled)
+				{
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+			}
+		}
 
         public string Result
         {
-            get;
-            set;
-        }
+            get
+			{
+                return _result;
+            }
+			set
+			{
+                if(value != _result)
+				{
+                    _result = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+                }
+			}
+		}
 
         #endregion
 
@@ -70,15 +130,92 @@
 
         public MainViewModel()
         {
+            LoadRates();
         }
 
         #endregion
 
         #region Methods
 
-        private void Convert()
+        /// <summary>
+        /// Metodo que hace la conversion de las tasas
+        /// </summary>
+        private async void Convert()
         {
-            throw new NotImplementedException();
+            if(string.IsNullOrEmpty(Amount))
+			{
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "You must enter a value in amount...!!!", "Accept");
+                return;
+            }
+
+            decimal amount = 0;
+            if(!decimal.TryParse(Amount, out amount))
+			{
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "You must enter a numeric value in amount...!!!", "Accept");
+                return;
+            }
+
+            if(SourceRate == null)
+			{
+				await Application.Current.MainPage.DisplayAlert(
+                    "Error", "You must select a source rate...!!!", "Accept");
+				return;
+			}
+
+			if (TargetRate == null)
+			{
+				await Application.Current.MainPage.DisplayAlert(
+                    "Error", "You must select a target rate...!!!", "Accept");
+				return;
+			}
+
+            var amountConvert = (amount / (decimal)SourceRate.TaxRate) 
+                * (decimal)TargetRate.TaxRate;
+
+            Result = string.Format("{0} {1:N2} = {2} {3:N2}", 
+                                   SourceRate.Code.Trim(),
+                                   amount,
+                                   TargetRate.Code,
+                                   amountConvert);
+		}
+
+        /// <summary>
+        /// Metodo que hace la carga de las tasas
+        /// </summary>
+		private async void LoadRates()
+		{
+            try
+            {
+				IsRunning = true;
+				Result = "Load rates, please wait...!!!";
+
+                var client = new System.Net.Http.HttpClient();
+                client.BaseAddress = new Uri("http://apiexchangerates.azurewebsites.net");
+                var controler = "/api/Rates";
+                var response = await client.GetAsync(controler);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if(response.IsSuccessStatusCode)
+				{
+                    IsRunning = false;
+                    Result = result.Trim();
+                }
+
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+
+                IsRunning = false;
+                IsEnabled = true;
+                Result = "Ready to convert...!!!";
+
+			}
+            catch (Exception ex)
+            {
+                IsRunning = false;
+                Result = ex.Message.Trim();
+            }
         }
 
 		#endregion
