4a9a2820cefb4c059f7ae2207ec31b799799f4f7
nivel10 <carlos.e.herrera.j@gmail.com>
2017-09-18 14:55:13 -0400

    Sincrinizacion - 18/09/2017 - 2:55

---
+++ ForeignExchangeMac/Models/Rate.cs
@@ -0,0 +1,13 @@
+﻿namespace ForeignExchangeMac.Models
+{
+    public class Rate
+    {
+		public int RateId { get; set; }
+
+		public string Code { get; set; }
+
+		public double TaxRate { get; set; }
+
+		public string Name { get; set; }
+    }
+}
--- ForeignExchangeMac/ViewModels/MainViewModel.cs
+++ ForeignExchangeMac/ViewModels/MainViewModel.cs
@@ -1,11 +1,86 @@
 ﻿namespace ForeignExchangeMac.ViewModels
 {
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+
     public class MainViewModel
     {
+        #region Commands
+
+        public ICommand ConvertCommand
+        {
+            get
+            {
+                return new RelayCommand(Convert);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Amount
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Rate> Rates
+        {
+            get;
+            set;
+        }
+
+        public Rate SourceRate
+        {
+            get;
+            set;
+        }
+
+        public Rate TargetRate
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
+
+        public string Result
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
         #region Constructor
+
         public MainViewModel()
         {
         }
+
         #endregion
+
+        #region Methods
+
+        private void Convert()
+        {
+            throw new NotImplementedException();
+        }
+
+		#endregion
     }
 }
